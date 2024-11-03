39288881aa7514d7cdcd3f0b352a4a6ed5eb9058
tommasotode <tommasotodescato@gmail.com>
2024-11-03 18:21:25 +0100

    total distribution plot

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.display_table, name='display_table'),
     path('athlete/<str:name>/', views.athlete_view, name='athlete_view'),
+    path('distribution/', views.distribution, name='distribution')
 ]
